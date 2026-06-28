d8037e01cd2e9f5e4fc35de6f4eadcec0f6b3499
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2026-06-28 20:25:00 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import './index.css'
 import { ToastProvider } from './context/ToastContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    // <React.StrictMode>
+    <React.StrictMode>
         <ToastProvider>
         <App />
         </ToastProvider>
-    // </React.StrictMode>,
+    </React.StrictMode>,
 )
