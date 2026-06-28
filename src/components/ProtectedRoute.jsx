581217c0dd6bb97f99065504889f9311e361ffd1
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2026-06-28 20:25:47 +0100

    Create ProtectedRoute.jsx

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,22 @@
+// components/ProtectedRoute.jsx
+import { Navigate } from 'react-router-dom'
+import { useToastActions } from '../hooks/useToastActions'
+import { useEffect } from "react";
+
+const ProtectedRoute = ({ children }) => {
+    const toast = useToastActions()
+    const isAuthenticated = localStorage.getItem('accessData')
+    // const isAuthenticated = true
+    console.log(`isAuthenticated: ${isAuthenticated}`)
+
+    if (!isAuthenticated) {
+        useEffect(() => {
+            toast.showError("You need to be logged in to access this page.")
+        }, [])
+        return <Navigate to="/auth/login" replace />
+    }
+
+    return children
+}
+
+export default ProtectedRoute
