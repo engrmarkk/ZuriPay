a04b908e24f28651e7ca83b8c5f332d3af510570
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2026-06-30 22:56:39 +0100

    Update ProtectedRoute.jsx

--- src/middlewares/ProtectedRoute.jsx
+++ src/middlewares/ProtectedRoute.jsx
@@ -1,20 +1,29 @@
 // components/ProtectedRoute.jsx
 import { Navigate } from 'react-router-dom'
 import { useToastActions } from '../hooks/useToastActions.js'
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 const ProtectedRoute = ({ children }) => {
     const toast = useToastActions()
     const isAuthenticated = localStorage.getItem('accessData')
+
+    const hasShownToast = useRef(false)
+
     // const isAuthenticated = true
     console.log(`isAuthenticated: ${isAuthenticated}`)
+    const isFromLogout = sessionStorage.getItem('isLoggingOut') === 'true'
 
     if (!isAuthenticated) {
         useEffect(() => {
-            toast.showError("You need to be logged in to access this page.")
+            if (!hasShownToast.current && !isFromLogout) {
+                toast.showError("You need to be logged in to access this page.")
+                hasShownToast.current = true
+            }
+            sessionStorage.removeItem('isLoggingOut')
         }, [])
         return <Navigate to="/auth/login" replace />
     }
+    hasShownToast.current = false
 
     return children
 }
